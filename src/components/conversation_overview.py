f8011094ca4750cf48012dfc7b18b12942632a44
rogerwaldvogel <climberwoodi@gmx.ch>
2022-11-26 17:39:31 +0100

    First draft using dash plotly. Only a simple list of the last conversations is implemented.

---
+++ src/components/conversation_overview.py
@@ -0,0 +1,18 @@
+import dash_bootstrap_components as dbc
+from dash import html
+from datetime import datetime
+
+
+def _get_title(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_conversations(df_conversations):
+    return html.Div(
+        [
+            dbc.ListGroup(
+                [dbc.ListGroupItem(f"{_get_title(x[1]['timestamp'])} - {x[1]['number_of_chats']}") for x in
+                 df_conversations.iterrows()]
+            )
+        ]
+    )
