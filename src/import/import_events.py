af5db66029ce49f0d5c6ada215f7323ddb41fa92
rogerwaldvogel <climberwoodi@gmx.ch>
2022-11-26 14:29:26 +0100

    Added cleaning of data.

--- src/import/import_events.py
+++ src/import/import_events.py
@@ -1,8 +1,32 @@
 import os
 import pandas as pd
+import json
 from connect_db import create_connection
 
 DATA_DIRECTORY = './../../data/'
+
+
+def add_data(cell, name):
+    data = json.loads(cell[0])
+    if name in data:
+        return data[name]
+
+
+def add_model_id(cell):
+    data = json.loads(cell[0])
+    return data['metadata']['model_id']
+
+
+def add_model_intent(cell, text):
+    data = json.loads(cell[0])
+    if 'parse_data' in data:
+        return data['parse_data']['intent'][text]
+
+
+def add_model_intent_ranking(cell):
+    data = json.loads(cell[0])
+    if 'parse_data' in data:
+        return data['parse_data']['intent_ranking']
 
 
 def import_events():
@@ -12,11 +36,25 @@ def import_events():
     db_host = os.environ.get('DB_HOST', '127.0.0.1')
     db_port = os.environ.get('DB_PORT', '5432')
     with create_connection(db_name, db_user, db_password, db_host, db_port) as con:
-        sql_query = pd.read_sql('SELECT * FROM events', con)
+        sql_query = pd.read_sql("SELECT * FROM events WHERE type_name = 'user' OR type_name = 'bot'", con)
         df = pd.DataFrame(sql_query,
                           columns=['sender_id', 'type_name', 'timestamp', 'intent_name', 'action_name', 'data'])
-        os.mkdir(DATA_DIRECTORY)
-        df.to_excel(f'{DATA_DIRECTORY}conversations.xlsx', sheet_name='conversations')
+
+        # Clean Data
+        df['model_id'] = df[['data']].apply(add_model_id, axis=1)
+        df['text'] = df[['data']].apply(lambda x: add_data(x, 'text'), axis=1)
+        df['intent'] = df[['data']].apply(lambda x: add_model_intent(x, 'name'), axis=1)
+        df['intent_confidence'] = df[['data']].apply(lambda x: add_model_intent(x, 'confidence'), axis=1)
+        df['input_channel'] = df[['data']].apply(lambda x: add_data(x, 'input_channel'), axis=1)
+        df['intent_ranking'] = df[['data']].apply(add_model_intent_ranking, axis=1)
+
+        df = df.drop('data', axis=1)
+
+        if not os.path.exists(DATA_DIRECTORY):
+            os.mkdir(DATA_DIRECTORY)
+        with pd.ExcelWriter(f'{DATA_DIRECTORY}conversations.xlsx', engine="openpyxl", mode="a",
+                            if_sheet_exists="replace") as writer:
+            df.to_excel(writer, sheet_name='conversations')
 
 
 if __name__ == "__main__":
