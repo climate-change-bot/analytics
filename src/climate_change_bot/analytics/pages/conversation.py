0e3ff45a0c3209a4d1a449e928170287260732fe
roger <climberwoodi@gmx.ch>
2023-04-25 13:46:43 +0200

    Handling the case when the conversation id does not exist.

--- src/climate_change_bot/analytics/pages/conversation.py
+++ src/climate_change_bot/analytics/pages/conversation.py
@@ -1,4 +1,5 @@
 import dash
+import dash_bootstrap_components as dbc
 from dash import html, callback, dcc
 from dash.dependencies import Input, Output
 import pandas as pd
@@ -31,5 +32,18 @@ def update_conversation_content(data, pathname):
     conversation_id = re.search(r'/conversation/(\w+)', pathname).group(1)
     if conversation_id and int(conversation_id) >= 0:
         df_conversation = df[df['conversation_id'] == int(conversation_id)]
-
-        return get_conversation_messages(df_conversation), get_side_bar(df_conversation)
+        if len(df_conversation):
+            return get_conversation_messages(df_conversation), get_side_bar(df_conversation)
+        else:
+            return html.Div([
+                dbc.Card(
+                    dbc.CardBody(
+                        [
+                            html.H5(f"The conversation id {conversation_id} does not exist"),
+                            dbc.CardLink("Previous", href=f"{int(conversation_id) - 1}"),
+                            dbc.CardLink("Next", href=f"{int(conversation_id) + 1}")
+                        ]
+                    ),
+                    style={"width": "auto", "margin": "12px 0px"}
+                )
+            ]), []
