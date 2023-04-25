437d16eb4e78647c808b68004b97a5294f5091cc
roger <climberwoodi@gmx.ch>
2023-04-25 13:00:02 +0200

    Added handling of further attempts.

--- src/climate_change_bot/analytics/components/quiz/quiz_analysis.py
+++ src/climate_change_bot/analytics/components/quiz/quiz_analysis.py
@@ -15,7 +15,7 @@ def _count_correct_answers(answers):
 
 
 def _get_correct_answer_statistic(df_quiz_answers):
-    results = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    results = {0: [0, 0], 1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0], 5: [0, 0], 6: [0, 0], 7: [0, 0], 8: [0, 0]}
     for conversation_id, group in df_quiz_answers.groupby('conversation_id'):
         group_size = len(group)
         for i in range(0, group_size, 8):
@@ -23,7 +23,7 @@ def _get_correct_answer_statistic(df_quiz_answers):
                 break
             answers = group['text'][i:i + 8]
             correct_count = _count_correct_answers(answers)
-            results[correct_count] += 1
+            results[correct_count][0 if i == 0 else 1] += 1
 
     return results
 
@@ -33,15 +33,18 @@ def get_number_of_correct_answer(df):
     answer_counts = _get_correct_answer_statistic(df_quiz_answers)
 
     x = [str(key) for key in answer_counts.keys()]
-    y = list(answer_counts.values())
+    y0 = [value[0] for value in answer_counts.values()]
+    y1 = [value[1] for value in answer_counts.values()]
 
-    fig = go.Figure(
-        data=[go.Bar(x=x, y=y)]
-    )
+    fig = go.Figure(data=[
+        go.Bar(name='First Attempt', x=x, y=y0, marker_color="#636EFA"),
+        go.Bar(name='Further Attempts', x=x, y=y1, marker_color="#EF553B")
+    ])
 
     fig.update_layout(
         xaxis=dict(title="Number of correct answers per quiz", fixedrange=True),
-        yaxis=dict(title="Frequency", fixedrange=True)
+        yaxis=dict(title="Frequency", fixedrange=True),
+        barmode="stack"
     )
 
     graph = dcc.Graph(id='quiz-answer-distribution-chart', figure=fig)
