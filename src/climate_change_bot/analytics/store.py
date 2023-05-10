4808ba444917a614fa8b7cc22161fc5f901a81d9
roger <climberwoodi@gmx.ch>
2023-05-10 17:27:42 +0200

    Remove testphase 0 from statistic.

--- src/climate_change_bot/analytics/store.py
+++ src/climate_change_bot/analytics/store.py
@@ -6,14 +6,17 @@ _file_name = os.environ.get('FILE_NAME', '../../../data/conversations_prod.xlsx'
 
 class GlobalStore:
     def __init__(self):
-        self.df = pd.read_excel(_file_name)
+        df = pd.read_excel(_file_name)
+        df = df[df.testphase != 0]  # Testphase 0 is ignored because it consists mainly of test conversations
+        df.reset_index(drop=True, inplace=True)
+        self.df = df
 
     def get_data(self, filter_value):
         if filter_value == 0:
             return self.df
         else:
             df_filtered = self.df[self.df.testphase == filter_value]
-            df_filtered.reset_index(inplace=True)
+            df_filtered.reset_index(drop=True, inplace=True)
             return df_filtered
 
     def set_data(self, new_df):
